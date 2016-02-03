9ad709cbd416e2a7c00aee1eee7d962fc78e3563
Jake Overall <joverall22@gmail.com>
2016-02-03 11:19:33 -0700

    initial commit

---
+++ app/components/todo/todo-controller.js
@@ -0,0 +1,46 @@
+app.controller('TodoController', function($scope){
+    $scope.test = "I like bananas"
+    
+    /** TASK MODEL
+     * {
+     *   task: String,
+     *   dueDate: Date,
+     *   responsibleParty: String,
+     *   estTime: Number
+     * }
+     */
+    
+    $scope.todos = [{
+        task: 'Walk Dog',
+        dueDate: Date.now(),
+        responsibleParty: 'Chance',
+        estTime: 15,
+    },{
+        task: 'Toilet Duty',
+        dueDate: new Date(2022, 4, 1),
+        responsibleParty: 'John',
+        estTime: 15,
+    },{
+        task: 'Pop Popcorn',
+        dueDate: Date.now(),
+        responsibleParty: 'Chance',
+        estTime: 10,
+    },{
+        task: 'Write CODE!!!',
+        dueDate: new Date(2016, 5, 28),
+        responsibleParty: 'Class',
+        estTime: 1500,
+    }];
+    
+    $scope.addItem = function(){
+        if($scope.newTask){
+            $scope.todos.push($scope.newTask);
+            $scope.newTask = '';
+        }
+    }
+    
+    $scope.removeItem = function(i){
+        $scope.todos.splice(i, 1);
+    }
+    
+})
